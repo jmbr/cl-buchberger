9e4d3981709d8d364e0b09232e4807265f3f1fbf
Juan M. Bello Rivas <jmbr@superadditive.com>
2007-10-22 00:09:36 +0200

    Removed bogus comment.
    committer: Juan M. Bello Rivas <jmbr@superadditive.com>

--- ring-element.lisp
+++ ring-element.lisp
@@ -16,7 +16,7 @@
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 (defclass ring-element ()
-  (base-ring)				; XXX use initform, ...
+  (base-ring)
   (:documentation "Base class for ring elements."))
 
 (defgeneric ring-copy (element)
